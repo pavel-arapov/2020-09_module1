245dda9316a2226e390aaefe7c62c80f13a14c47
pavel <pavel.arapov@gmail.com>
2020-10-20 22:38:46 +0100

    feat(): home work 3 + initial sounds, click on button

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -9,42 +9,46 @@ public class MenuController : MonoBehaviour
     {
         None,
         Main,
+        Levels,
         Settings,
     }
 
     public CanvasGroup mainScreen;
     public CanvasGroup settingsScreen;
+    public CanvasGroup levelsScreen;
 
-    void SetCurrentScreen(Screen screen)
-    {
+    void SetCurrentScreen(Screen screen) {
         Utility.SetCanvasGroupEnabled(mainScreen, screen == Screen.Main);
         Utility.SetCanvasGroupEnabled(settingsScreen, screen == Screen.Settings);
+        Utility.SetCanvasGroupEnabled(levelsScreen, screen == Screen.Levels);
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         SetCurrentScreen(Screen.Main);
     }
 
-    public void StartNewGame()
-    {
-        SetCurrentScreen(Screen.None);
-        LoadingScreen.instance.LoadScene("SampleScene");
+    public void StartNewGame() {
+        SetCurrentScreen(Screen.Levels);
     }
 
-    public void OpenSettings()
-    {
+    /**
+     * TODO: level parameter has to be verified 
+     */
+    public void SelectLevel(string level) {
+        SetCurrentScreen(Screen.None);
+        LoadingScreen.instance.LoadScene(level);
+    }
+
+    public void OpenSettings() {
         SetCurrentScreen(Screen.Settings);
     }
 
-    public void CloseSettings()
-    {
+    public void ReturnToMainMenu() {
         SetCurrentScreen(Screen.Main);
     }
 
-    public void ExitGame()
-    {
+    public void ExitGame() {
         Application.Quit();
     }
 }
